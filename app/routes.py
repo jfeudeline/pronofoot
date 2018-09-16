ae24e301a9ab6b94f971dd36e2adc73031d8a231
jfeudeline <jfeudeline@gmail.com>
2018-09-16 17:35:46 +0200

    Add index template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
 from app import app
 
+from flask import render_template
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'coucou'
+    user = {'username': 'jef'}
+
+    teams = [
+        {'name': 'Paris SG', 'rank': 1},
+        {'name': 'SM Caen',  'rank': 12},
+        {'name': 'OM',       'rank': 4},
+        {'name': 'OL',       'rank': 9},
+    ]
+    matchs = [
+        {'team_A': teams[0], 'team_B': teams[1]},
+        {'team_A': teams[2], 'team_B': teams[3]}
+    ]
+
+    return render_template('index.html', user = user, matchs = matchs, title = "Home")
